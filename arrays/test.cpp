810818deca2fccde249de172423d9295a44f6dc3
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-07 11:57:16 +0530

    added:test case added

--- arrays/test.cpp
+++ arrays/test.cpp
@@ -4,22 +4,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int maxSubArraySum(vector<int>& arr) {
-    int max_current = arr[0];
-    int max_global = arr[0];
-    
-    for (int i = 1; i < arr.size(); i++) {
-        max_current = max(arr[i], max_current + arr[i]);
-        if (max_current > max_global) {
-            max_global = max_current;
-        }
-    }
-    
-    return max_global;
-}
+
 
 int main() {
-    vector<int> arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-    cout << "Maximum subarray sum is: " << maxSubArraySum(arr) << endl;
-    return 0;
+    vector<float> arr = {0.76, 0.74, 0.12, 0.15, 0.54, 0.42, 0.95, 0.87, 0.85};
+    int size = arr.size();
+    cout<<"Original array: ";
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    vector<vector<float>> b(size);
+    cout<<endl;
+    //Put array elements in different buckets
+    for(int i=0;i<size;i++){
+        int bi = size * arr[i];
+        cout << bi <<" ";
+    }
+    
 }
