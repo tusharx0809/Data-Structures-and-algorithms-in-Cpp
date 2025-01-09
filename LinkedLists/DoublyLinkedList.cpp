1fe73194fa4acd162ce84e37dee74751423fba4a
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-09 11:10:47 +0530

    added: insert at position added in Doubly linkedList

--- LinkedLists/DoublyLinkedList.cpp
+++ LinkedLists/DoublyLinkedList.cpp
@@ -34,24 +34,51 @@ public:
             head = newNode;
         }
     }
-    void insertAtEnd(int value){
-        Node* newNode = new Node();
+    void insertAtEnd(int value)
+    {
+        Node *newNode = new Node();
         newNode->data = value;
         newNode->next = NULL;
         newNode->prev = NULL;
 
-        if(!head){
+        if (!head)
+        {
             head = newNode;
         }
-        Node* temp = head;
+        Node *temp = head;
 
-        while(temp->next != NULL){
+        while (temp->next != NULL)
+        {
             temp = temp->next;
         }
         temp->next = newNode;
         newNode->prev = temp;
     }
+    void insertAtPosition(int value, int position)
+    {
+        Node* newNode = new Node();
+        newNode->data = value;
+        newNode->next = NULL;
+        newNode->prev = NULL;
 
+        if (!head)
+        {
+            cout << "List is empty" << endl;
+        }
+        if(head==NULL || position == 1){
+            newNode->next = head;
+            head = newNode;
+        }
+        int pos = 1;
+        Node * temp = head;
+        while(position-1 != pos){
+            pos++;
+            temp = temp->next;
+        }
+        newNode->next = temp->next;
+        newNode->prev = temp;
+        temp->next = newNode;
+    }
 
     void display()
     {
@@ -74,24 +101,27 @@ public:
 int main()
 {
     DoublyLinkedList list1;
-    list1.display();//List is empty!
+    list1.display(); // List is empty!
 
     for (int i = 0; i < 8; i += 2)
     {
-        if (i > 0) //Adding even numbers greater than 0 and less than 8
+        if (i > 0) // Adding even numbers greater than 0 and less than 8
         {
             list1.insertAtBeginning(i);
         }
     }
-    list1.display(); //NULL <-> 6 <-> 4 <-> 2 <-> NULL
+    list1.display(); // NULL <-> 6 <-> 4 <-> 2 <-> NULL
 
     for (int i = 0; i < 8; i += 2)
     {
-        if (i > 0) //Adding even numbers greater than 0 and less than 8
+        if (i > 0) // Adding even numbers greater than 0 and less than 8
         {
             list1.insertAtEnd(i);
         }
     }
-    list1.display(); //NULL <-> 6 <-> 4 <-> 2 <-> 2 <-> 4 <-> 6 <-> NULL
+    list1.display(); // NULL <-> 6 <-> 4 <-> 2 <-> 2 <-> 4 <-> 6 <-> NULL
+
+    list1.insertAtPosition(5, 2);
+    list1.display(); //NULL <-> 6 <-> 5 <-> 4 <-> 2 <-> 2 <-> 4 <-> 6 <-> NULL
     return 0;
 }
