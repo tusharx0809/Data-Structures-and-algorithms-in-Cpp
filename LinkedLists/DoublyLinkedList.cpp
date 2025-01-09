0dc72543b84169968301dc5e16cdf6a6ef9ae089
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-09 10:53:49 +0530

    added: insert at end added in doubly linked list

--- LinkedLists/DoublyLinkedList.cpp
+++ LinkedLists/DoublyLinkedList.cpp
@@ -34,12 +34,31 @@ public:
             head = newNode;
         }
     }
+    void insertAtEnd(int value){
+        Node* newNode = new Node();
+        newNode->data = value;
+        newNode->next = NULL;
+        newNode->prev = NULL;
+
+        if(!head){
+            head = newNode;
+        }
+        Node* temp = head;
+
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        newNode->prev = temp;
+    }
+
 
     void display()
     {
         if (!head)
         {
             cout << "List is empty!" << endl;
+            return;
         }
         Node *temp = head;
         cout << "NULL <-> ";
@@ -55,15 +74,24 @@ public:
 int main()
 {
     DoublyLinkedList list1;
-    list1.display();
+    list1.display();//List is empty!
 
     for (int i = 0; i < 8; i += 2)
     {
-        if (i > 0)
+        if (i > 0) //Adding even numbers greater than 0 and less than 8
         {
             list1.insertAtBeginning(i);
         }
     }
-    list1.display();
+    list1.display(); //NULL <-> 6 <-> 4 <-> 2 <-> NULL
+
+    for (int i = 0; i < 8; i += 2)
+    {
+        if (i > 0) //Adding even numbers greater than 0 and less than 8
+        {
+            list1.insertAtEnd(i);
+        }
+    }
+    list1.display(); //NULL <-> 6 <-> 4 <-> 2 <-> 2 <-> 4 <-> 6 <-> NULL
     return 0;
 }
