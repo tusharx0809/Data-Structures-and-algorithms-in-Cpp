cb086f80dc97ff8a3a3aa2437e67870301bbbe94
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-14 16:33:32 +0530

    fixed: postfix to prefix fixed

--- stacks/PostfixToPrefix.cpp
+++ stacks/PostfixToPrefix.cpp
@@ -1,54 +1,49 @@
 #include <iostream>
 #include <stack>
-#include <string>
-#include <cctype>
 #include <sstream>
+#include <string>
 using namespace std;
 
-// Function to check if a character is an operator
-bool isOperator(char c) {
-    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
+// Function to check if a token is an operator
+bool isOperator(const string& token) {
+    return (token == "+" || token == "-" || token == "*" || token == "/" || token == "^");
 }
 
 // Function to convert postfix to prefix
 string postfixToPrefix(string postfix) {
     stack<string> s;
+    stringstream ss(postfix);
+    string token;
 
-    // Traverse the postfix expression from left to right
-    for (int i = 0; i < postfix.length(); i++) {
-        // If the character is an operator
-        if (isOperator(postfix[i])) {
+    // Split the input into tokens using a space delimiter
+    while (ss >> token) {
+        // If the token is an operator
+        if (isOperator(token)) {
             // Pop two operands from the stack
-            if(s.size() < 2) return "Invalid Expression";
-            
-            string operand2 = s.top(); s.pop();
+            if (s.size() < 2) return "Invalid Expression";
+
             string operand1 = s.top(); s.pop();
+            string operand2 = s.top(); s.pop();
 
             // Concatenate the operator and the operands in prefix order
-            string prefix = postfix[i] + operand1 + operand2;
+            string prefix = token + " " + operand2 + " " + operand1;
 
             // Push the result back onto the stack
             s.push(prefix);
         }
-        // If the character is a digit, handle multiple digits
-        else if (isdigit(postfix[i])) {
-            string num = "";
-            // Read the whole number (in case of multiple digits)
-            while (i < postfix.length() && isdigit(postfix[i])) {
-                num += postfix[i++];
-            }
-            i--; // Decrease index because the loop will increment it
-            s.push(num);
+        // If the token is an operand
+        else {
+            // Push the operand directly onto the stack
+            s.push(token);
         }
     }
 
     // The final element in the stack is the prefix expression
-    return s.top();
+    return s.size() == 1 ? s.top() : "Invalid Expression";
 }
 
 int main() {
     string postfix = "A B C / - A K / L - *";
-    
 
     string prefix = postfixToPrefix(postfix);
     cout << "The prefix expression is: " << prefix << endl;
