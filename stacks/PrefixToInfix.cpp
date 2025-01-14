988677334329a639981de448d2ca3e296e5a48c2
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-14 10:27:31 +0530

    added: handling of multiple digits added in prefix to infix

--- stacks/PrefixToInfix.cpp
+++ stacks/PrefixToInfix.cpp
@@ -1,47 +1,68 @@
-// Prefix to infix to conversion
-
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isOperator(char x)
-{
-    switch (x)
-    {
+// Check if the character is an operator
+bool isOperator(char x) {
+    switch (x) {
         case '+':
         case '-':
         case '/':
         case '*':
         case '^':
         case '%':
-        return true;
+            return true;
     }
     return false;
 }
 
-string prefixToInfix(string exp){
-    stack <string> s;
-
+// Function to convert prefix to infix
+string prefixToInfix(string exp) {
+    stack<string> s;
     int length = exp.size();
 
-    for(int i = length-1; i>=0 ; i--){
-        if(isOperator(exp[i])){
+    // Start from the end of the string and process from right to left
+    for (int i = length - 1; i >= 0; ) {
+        // Skip spaces (if present in the input)
+        if (exp[i] == ' ') {
+            i--;
+            continue;
+        }
+
+        // If the current character is an operator
+        if (isOperator(exp[i])) {
             string op1 = s.top();
             s.pop();
             string op2 = s.top();
             s.pop();
 
+            // Create the infix expression with parentheses
             string temp = "(" + op1 + exp[i] + op2 + ")";
-
             s.push(temp);
-        }else{
-            s.push(string(1, exp[i]));
+
+            i--; // Move to the next character
+        } else {
+            // For multi-character operands, extract the entire operand
+            string operand = "";
+
+            // Traverse backward to find the full operand
+            while (i >= 0 && !isOperator(exp[i]) && exp[i] != ' ') {
+                operand = exp[i] + operand; // Add character to the front of the operand
+                i--;
+            }
+
+            // Push the complete operand to the stack
+            s.push(operand);
         }
     }
+
+    // The final result is the only element left in the stack
     return s.top();
 }
 
-int main(){
-    string s = "*-A/BC-/AKL";
-    cout<<"Infix: "<< prefixToInfix(s);
-    return 0; 
+int main() {
+    string s = "* - A / B C - / A K L";
+    cout << "Infix: " << prefixToInfix(s) << endl;
+    string s2 = "- + 10 * 20 ^ - ^ 30 4 50 + 6 * 70 8 90";
+    cout <<"Infix: "<<prefixToInfix(s2) <<endl;
+    return 0;
 }
