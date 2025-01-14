8341bd8f3b39c530cef76a7347d47b4649289f9c
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-14 12:24:17 +0530

    added: test cases added

--- stacks/PostfixToPrefix.cpp
+++ stacks/PostfixToPrefix.cpp
@@ -1,69 +1,55 @@
-//Prefix to Postfix
-
-#include <bits/stdc++.h>
+#include <iostream>
+#include <stack>
+#include <string>
+#include <cctype>
+#include <sstream>
 using namespace std;
 
-// Check if the character is an operator
-bool isOperator(char x) {
-    switch (x) {
-        case '+':
-        case '-':
-        case '/':
-        case '*':
-        case '^':
-        case '%':
-            return true;
-    }
-    return false;
+// Function to check if a character is an operator
+bool isOperator(char c) {
+    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
 }
 
-// Function to convert prefix to infix
-string postfixToPrefix(string exp) {
+// Function to convert postfix to prefix
+string postfixToPrefix(string postfix) {
     stack<string> s;
-    int length = exp.size();
 
-    // Start from the end of the string and process from right to left
-    for (int i = 0; i < length; i++ ) {
-        // Skip spaces (if present in the input)
-        if (exp[i] == ' ') {
-            continue;
+    // Traverse the postfix expression from left to right
+    for (int i = 0; i < postfix.length(); i++) {
+        // If the character is an operator
+        if (isOperator(postfix[i])) {
+            // Pop two operands from the stack
+            string operand2 = s.top(); s.pop();
+            string operand1 = s.top(); s.pop();
+
+            // Concatenate the operator and the operands in prefix order
+            string prefix = postfix[i] + operand1 + operand2;
+
+            // Push the result back onto the stack
+            s.push(prefix);
         }
-
-        // If the current character is an operator
-        if (isOperator(exp[i])) {
-            string op1 = s.top();
-            s.pop();
-            string op2 = s.top();
-            s.pop();
-
-            // Create the infix expression with parentheses
-            string temp = exp[i] + " " + op2 + " " + op1;
-            s.push(temp);
-
- 
-        } else {
-            // For multi-character operands, extract the entire operand
-            string operand = "";
-
-            // Traverse backward to find the full operand
-            while (i < length && !isOperator(exp[i]) && exp[i] != ' ') {
-                operand = exp[i] + operand; // Add character to the front of the operand
-                i++;
+        // If the character is a digit, handle multiple digits
+        else if (isdigit(postfix[i])) {
+            string num = "";
+            // Read the whole number (in case of multiple digits)
+            while (i < postfix.length() && isdigit(postfix[i])) {
+                num += postfix[i++];
             }
-
-            i--;
-            s.push(operand);
+            i--; // Decrease index because the loop will increment it
+            s.push(num);
         }
     }
 
-    
+    // The final element in the stack is the prefix expression
     return s.top();
 }
 
 int main() {
-    string s = "A B C / - A K / L - *";
-    cout << "Prefix: " << postfixToPrefix(s) << endl; //A B C / - A K / L - *
-    string s2 = "10 20 30 4 ^ 50 - 6 70 8 * + ^ * + 90 -"; 
-    cout <<"Prefix: "<<postfixToPrefix(s2) <<endl; //10 20 30 4 ^ 50 - 6 70 8 * + ^ * + 90 -
+    string postfix = "A B C / - A K / L - *";
+    
+
+    string prefix = postfixToPrefix(postfix);
+    cout << "The prefix expression is: " << prefix << endl;
+
     return 0;
 }
