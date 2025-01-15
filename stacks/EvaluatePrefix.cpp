2c4e81e56d09c0549b519526d7ce5b67eae334aa
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-15 12:34:00 +0530

    fixed: fixed evaluate prefix

--- stacks/EvaluatePrefix.cpp
+++ stacks/EvaluatePrefix.cpp
@@ -6,8 +6,8 @@ bool isOperator(char x) {
 }
 
 float evaluatePrefix(string exp) {
-    stack<float> s;
-    string num;
+    stack<float> st;
+    string num="";
 
     for(int i=exp.length()-1; i>=0; i--){
         char c = exp[i];
@@ -16,12 +16,44 @@ float evaluatePrefix(string exp) {
         }
         if(isdigit(c) || c == '.'){
             num = c + num;
+            if(i-1 < 0 || (!isdigit(exp[i-1]) && exp[i-1] != '.')){
+                st.push(stof(num));
+                num="";
+            }
+        }else{
+            if(st.size() < 2){
+                cout<<"Infsufficient operands";
+                return -1;
+            }
+            float num2 = st.top();
+            st.pop();
+            float num1 = st.top();
+            st.pop();
+            switch (exp[i])
+            {
+                case '+': 
+                    st.push(num2 + num1);
+                    break;
+                case '-':
+                    st.push(num2 - num1);
+                    break;
+                case '/':
+                    st.push(num2 / num1);
+                    break;
+                case '*':
+                    st.push(num2 * num1);
+                    break;
+                case '^':
+                    st.push(pow(num2, num1));
+                    break;
+            }
         }
     }
+    return st.top();
 }
 
 int main() {
-    string exp = "+ 3.5 * ^ 4 2 - 6 2";  // Example: 3.5 + (4^2 * (6-2)) = 67.5
+    string exp = "+ 3.5 * ^ 4 2 - 16 2";  // Example: 3.5 + (4^2 * (16-2)) = 227.5
     float result = evaluatePrefix(exp);
     cout<<result<<endl;
     return 0;
