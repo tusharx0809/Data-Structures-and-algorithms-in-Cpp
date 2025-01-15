a41833e5c3f9da765698c6251089488cb25da589
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-15 10:59:08 +0530

    added: added reverse floating number in prefix evaluation

---
+++ stacks/EvaluatePrefix.cpp
@@ -0,0 +1,25 @@
+//CPP program to evaluate Prefix expressions
+
+#include<bits/stdc++.h>
+using namespace std;
+bool isOperator(char x){
+    return (x == '^' || x == '/' || x == '*' || x == '+' || x == '-');
+}
+string floatingNumber(string exp){
+    string result="";
+    for(int i = exp.length()-1; i >=0; i--){
+        result += exp[i];
+    }
+    return result;
+
+}
+int main(){
+    // string exp = "+ 3.5 * ^ 4 2 - 6 2";
+    // float result = evaluatePrefix(exp);
+    // cout<<result;
+
+    string exp = "5.3";
+    string res = floatingNumber(exp);
+    cout<<res;
+    return 0;
+}
