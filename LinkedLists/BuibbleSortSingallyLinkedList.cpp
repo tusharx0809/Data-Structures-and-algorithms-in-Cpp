9793c19a5239a088c173a2fb3c444dd9b516729c
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-16 13:13:22 +0530

    added: added bubble sort in linked list

--- LinkedLists/BuibbleSortSingallyLinkedList.cpp
+++ LinkedLists/BuibbleSortSingallyLinkedList.cpp
@@ -26,6 +26,54 @@ class LinkedList{
             newNode->next = head;
             head = newNode;
         }
+
+        int getLength(){
+            int len = 0;
+            Node *temp = head;
+            while(temp){
+                len++;
+                temp = temp->next;
+            }
+            return len;
+        }
+
+        void bubbleSort(){
+            int len = getLength();
+            int itr = 0;
+            bool swapped;
+
+            while(itr < len){
+                Node *traverseNode = head;
+                Node *prevNode = head;
+                swapped = false;
+
+                while(traverseNode->next){
+                    Node *ptr = traverseNode->next;
+                    if(traverseNode->data > ptr->data){
+                        swapped = true;
+                        if(traverseNode == head){
+                            traverseNode->next = ptr->next;
+                            ptr->next = traverseNode;
+                            prevNode = ptr;
+                            head = prevNode;
+                        }else{
+                            traverseNode->next = ptr->next;
+                            ptr->next = traverseNode;
+                            prevNode->next = ptr;
+                            prevNode = ptr;
+                        }
+                        continue;
+                    }
+                    prevNode = traverseNode;
+                    traverseNode = traverseNode->next;
+                }
+                if(!swapped){
+                    break;
+                }
+                itr++;
+            }
+        }
+
         void display(){
             if(!head){
                 cout<<"Empty List!"<<endl;
@@ -36,17 +84,21 @@ class LinkedList{
                 cout<<temp->data<<" -> ";
                 temp=temp->next;
             }
-            cout<<"NULL";
+            cout<<"NULL"<<endl;
         }
 };
 
 int main(){
     LinkedList list;
-    list.display();
+    list.display(); //Empty List!
 
     for(int i=0; i<=5 ; i++){
         list.insertAtBeginning(i);
     }
-    list.display();
+    list.display(); //5 -> 4 -> 3 -> 2 -> 1 -> 0 -> NULL
+
+    list.bubbleSort();
+    list.display(); //0 -> 1 -> 2 -> 3 -> 4 -> 5 -> NULL
+
     return 0;
 }
