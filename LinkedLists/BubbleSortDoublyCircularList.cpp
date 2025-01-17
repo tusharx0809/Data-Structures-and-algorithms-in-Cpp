b7060142423837f852092284826e9448fc9eeca8
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-17 10:20:30 +0530

    added: added insert func in bubble sort doubly list

--- LinkedLists/BubbleSortDoublyCircularList.cpp
+++ LinkedLists/BubbleSortDoublyCircularList.cpp
@@ -19,11 +19,44 @@ class DoublyCircularList{
             Node *newNode = new Node();
             newNode->data = value;
             newNode->next = NULL;
-            newNode->pev = NULL;
+            newNode->prev = NULL;
 
             if(!head){
                 head = newNode;
                 newNode->next = head;
+                newNode->prev = head;
+                return;
             }
+            Node *temp = head;
+            while(temp->next != head){
+                temp = temp->next;
+            }
+            temp->next = newNode;
+            newNode->next = head;
+            newNode->prev = temp;
+            head = newNode;
+        }
+
+        void display(){
+            if(!head){
+                cout<<"Empty List!"<<endl;
+                return;
+            }
+            cout<<"HEAD <-> ";
+            Node *temp = head;
+            do{
+                cout<<temp->data<<" <-> ";
+                temp = temp->next;
+            }while(temp != head);
+            cout<<"HEAD"<<endl;
         }
 };
+
+int main(){
+    DoublyCircularList list;
+    for(int i=0; i<5; i++){
+        list.insertAtBeginning(i);
+    }
+    list.display();
+    return 0;
+}
