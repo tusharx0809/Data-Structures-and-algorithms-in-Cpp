1a83c4e1b4391718ddfc1255f3a98f9616c8077d
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-21 17:25:29 +0530

    added: added merge sort for doubly list

--- LinkedLists/Sorting/MergeSortDoublyLinkedList.cpp
+++ LinkedLists/Sorting/MergeSortDoublyLinkedList.cpp
@@ -28,6 +28,54 @@ class DoublyLinkedList{
         head->prev = newNode;
         head = newNode;
     }
+    Node *split(Node *head){
+        Node *fast = head;
+        Node *slow = head;
+
+        while(fast != NULL && fast->next != NULL && fast->next->next != NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+
+        Node *temp = slow->next;
+        slow->next = NULL;
+        if(temp != NULL){
+            temp->prev = NULL;
+        }
+        return temp;
+    }
+    Node *merge(Node *first, Node *second){
+        if(first == NULL) return second;
+        if(second == NULL) return first;
+
+        if(first->data < second->data){
+            first->next = merge(first->next, second);
+            if(first->next != NULL){
+                first->next->prev = first;
+            }
+            first->prev = NULL;
+            return first;
+        }else{
+            second->next = merge(first, second->next);
+            if(second->next != NULL){
+                second->next->prev = second;
+            }
+            second->prev = NULL;
+            return second;
+        }
+
+    }
+    Node *mergeSort(Node *head){
+        if(!head || !head->next){
+            return head;
+        }
+        Node *second = split(head);
+
+        head = mergeSort(head);
+        second = mergeSort(second);
+
+        return merge(head, second);
+    }
 
     void display(){
         if(!head){
@@ -40,17 +88,20 @@ class DoublyLinkedList{
             cout<<temp->data<<" <-> ";
             temp = temp->next;
         }
-        cout<<"NULL";
+        cout<<"NULL"<<endl;
     }
 };
 
 int main(){
     DoublyLinkedList list;
-    list.display();
+    list.display(); //Empty List!
 
     for(int i=0;i<5;i++){
         list.insertAtBeginning(i);
     }
+    list.display(); //NULL <-> 4 <-> 3 <-> 2 <-> 1 <-> 0 <-> NULL
+
+    list.head = list.mergeSort(list.head);
     list.display();
     return 0;
 }
