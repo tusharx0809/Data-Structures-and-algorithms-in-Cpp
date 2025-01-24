c3b814b5371b86ba522fb17315ed56c2b140222d
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-24 10:41:20 +0530

    enqueue function optimized in queue

--- Queues/QueueUsingLinkedList.cpp
+++ Queues/QueueUsingLinkedList.cpp
@@ -1,131 +1,145 @@
-//CPP program to implement queue using linked list
+// CPP program to implement queue using linked list
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct Node{
+struct Node
+{
     int data;
     struct Node *next;
 };
 
-class Queue{
+class Queue
+{
     Node *front;
     Node *tail;
     int size;
     int capacity;
 
-    public:
-        Queue(int c) : front(nullptr), tail(nullptr), size(0), capacity(c) {}
+public:
+    Queue(int c) : front(nullptr), tail(nullptr), size(0), capacity(c) {}
 
-        ~Queue() {
-            while (tail != nullptr) {
-                Node* temp = tail;  // Store the current front
-                tail = tail->next; // Move to the next node
-                delete temp;         // Delete the old front
-            }
-            front = nullptr; // Ensure tail is also nullptr after cleanup
+    ~Queue()
+    {
+        while (tail != nullptr)
+        {
+            Node *temp = tail; // Store the current front
+            tail = tail->next; // Move to the next node
+            delete temp;       // Delete the old front
+        }
+        front = nullptr; // Ensure tail is also nullptr after cleanup
+    }
+
+    void enqueue(int data)
+    {
+        if (size == capacity)
+        {
+            cout << "Queue is full, cannot insert!" << endl;
+            return;
         }
 
-        void enqueue(int data){
-            Node *newNode = new Node();
-            newNode->data = data;
-            newNode->next = nullptr;
+        Node *newNode = new Node();
+        newNode->data = data;
+        newNode->next = nullptr;
 
-            if(!tail){
-                tail = newNode;
-                front = newNode;
-                size++;
-                return;
-            }
-
-            if(size == capacity){
-                cout<<"Queue is full, cannot insert!"<<endl;
-                return;
-            }else{
-                newNode->next = tail;
-                tail = newNode;
-                size++;
-            }
-            Node *temp = tail;
-            while(temp->next != NULL){
-                temp = temp->next;
-            }
-            front = temp;              
+        if (!tail)
+        { // Queue is empty
+            tail = newNode;
+            front = newNode;
         }
-        
-        void getFront(){
-            if(!tail){
-                cout<<"Queue is empty"<<endl;
-                return;
-            }
-            cout<<front->data<<endl;
+        else
+        {
+            newNode->next = tail;
+            tail = newNode;
         }
 
-        void dequeue(){
-            if(!tail){
-                cout<<"Queue is already empty"<<endl;
-                return;
-            }
-            if(tail == front){
-                delete tail;
-                tail = nullptr;
-                front = nullptr;
-                size--;
-                return;
-            }
-            Node *delNode = front;
-            Node *temp = tail;
-            while(temp->next != front){
-                temp = temp->next;
-            }
-            front = temp;
-            front->next = nullptr;
-            delete delNode;
+        size++;
+    }
+
+    void getFront()
+    {
+        if (!tail)
+        {
+            cout << "Queue is empty" << endl;
+            return;
+        }
+        cout << "Front element of queue is: "<<front->data << endl;
+    }
+
+    void dequeue()
+    {
+        if (!tail)
+        {
+            cout << "Queue is already empty" << endl;
+            return;
+        }
+        if (tail == front)
+        {
+            delete tail;
+            tail = nullptr;
+            front = nullptr;
             size--;
+            return;
         }
-
-        void display(){
-            if(!tail){
-                cout<<"Queue is empty"<<endl;
-                return;
-            }
-            Node *temp= tail;
-            cout<<"TAIL ->";
-            while(temp != nullptr){
-                cout<<temp->data<<" -> ";
-                temp = temp->next;
-            }
-            cout<<"FRONT"<<endl;
+        Node *delNode = front;
+        Node *temp = tail;
+        while (temp->next != front)
+        {
+            temp = temp->next;
         }
+        front = temp;
+        front->next = nullptr;
+        delete delNode;
+        size--;
+    }
 
+    void display()
+    {
+        if (!tail)
+        {
+            cout << "Queue is empty" << endl;
+            return;
+        }
+        Node *temp = tail;
+        cout << "TAIL ->";
+        while (temp != nullptr)
+        {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "FRONT" << endl;
+    }
 };
 
-int main(){
+int main()
+{
 
-    Queue q(5); //Initialize queue of capacity 5
-    q.display(); //Queue is empty
+    Queue q(5);  // Initialize queue of capacity 5
+    q.display(); // Queue is empty
 
-    for(int i=0;i<5;i++){
+    for (int i = 0; i < 5; i++)
+    {
         q.enqueue(i);
     }
-    q.display(); //TAIL ->4 -> 3 -> 2 -> 1 -> 0 -> FRONT
-    q.enqueue(5); //Queue is full, cannot insert!
+    q.display();  // TAIL ->4 -> 3 -> 2 -> 1 -> 0 -> FRONT
+    q.enqueue(5); // Queue is full, cannot insert!
 
-    q.getFront(); //0
-    
+    q.getFront(); // Front element of queue is: 0
+
     q.dequeue();
-    q.display();  //TAIL ->4 -> 3 -> 2 -> 1 -> FRONT , 0 dequeued
+    q.display(); // TAIL ->4 -> 3 -> 2 -> 1 -> FRONT , 0 dequeued
 
-    q.getFront(); //1
+    q.getFront(); // Front element of queue is: 1
 
     q.enqueue(5);
-    q.display(); //TAIL ->5 -> 4 -> 3 -> 2 -> 1 -> FRONT
+    q.display(); // TAIL ->5 -> 4 -> 3 -> 2 -> 1 -> FRONT
 
-    for(int i=0;i<5;i++){
+    for (int i = 0; i < 5; i++)
+    {
         q.dequeue();
     }
-    q.display();
+    q.display(); // Queue is empty
 
-    q.dequeue();
+    q.dequeue(); // Queue is already empty
     return 0;
 }
