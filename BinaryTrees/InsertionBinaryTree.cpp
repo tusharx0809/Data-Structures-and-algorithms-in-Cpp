73811d4022970beaa79be1ad8463e40e37092f5a
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-30 10:21:55 +0530

    insertion in binary tree started

---
+++ BinaryTrees/InsertionBinaryTree.cpp
@@ -0,0 +1,22 @@
+//CPP program to insert in binary tree in level order
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public: 
+        int data;
+        Node *left;
+        Node *right;
+
+        Node(int val){
+            data = val;
+            left = right = nullptr;
+        }
+};
+
+
+
+int main(){
+    return 0;
+}
