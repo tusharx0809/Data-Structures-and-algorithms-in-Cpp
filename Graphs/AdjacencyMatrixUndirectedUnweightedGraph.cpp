c2be9cbb06c3b1ba08ee15f546d387cf4b5e40d1
tusharx0809 <tusharbhatia0809@outlook.com>
2025-02-05 16:37:08 +0530

    added: added adjacency matrix for undirected graph

---
+++ Graphs/AdjacencyMatrixUndirectedUnweightedGraph.cpp
@@ -0,0 +1,45 @@
+//C++ program to represent Adjacency Matrix
+//Representation of undirected and unweighted graph
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void addEdge(vector<vector<int>> &mat, int i, int j){
+    mat[i][j] = 1;
+    mat[j][i] = 1;
+}
+
+void displayMatrix(vector<vector<int>> &mat){
+    int V = mat.size();
+    for(int i=0;i<V;i++){
+        for(int j=0;j<V;j++){
+            cout<<mat[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    //Create a graph with 4 vertices and no edges
+    // All vertices are initialized as 0
+
+    int V = 4;
+    vector<vector<int>> mat(V, vector<int>(V, 0));
+
+    //add edges
+    addEdge(mat, 0, 1);
+    addEdge(mat, 0, 2);
+    addEdge(mat, 1, 2);
+    addEdge(mat, 2, 3);
+
+    cout<<"Adjaceny Matrix: "<<endl;
+    displayMatrix(mat);
+
+    /* mat = {{0, 1, 0, 0},
+              {1, 0, 1, 0},
+              {0, 1, 0, 1},
+              {0, 0, 1, 0}}*/
+
+    return 0;
+
+}
