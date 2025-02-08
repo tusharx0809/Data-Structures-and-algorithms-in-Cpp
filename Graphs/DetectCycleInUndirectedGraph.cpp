d75890a48c28789ab9d80bb414ed51d45ece4a10
tusharx0809 <tusharbhatia0809@outlook.com>
2025-02-08 18:24:30 +0530

    added: added detect cycle in undirected graph

--- Graphs/DetectCycleInUndirectedGraph.cpp
+++ Graphs/DetectCycleInUndirectedGraph.cpp
@@ -17,6 +17,33 @@ void printGraph(vector<vector<int>> &adj){
         cout<<endl;
     }
 }
+bool isCyclicUtil(int v, vector<vector<int>> &adj, vector<bool> &visited, int parent){
+    visited[v] = true;
+
+    for(int i: adj[v]){
+        if(!visited[i]){
+            if(isCyclicUtil(i, adj, visited, v)){
+                return true;
+            }
+        }else if(i != parent){
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isCyclic(vector<vector<int>> &adj, int vertices){
+    vector<bool> visited(vertices, false);
+
+    for(int i=0; i<vertices; i++){
+        if(!visited[i]){
+            if(isCyclicUtil(i, adj, visited, -1)){
+                return true;
+            }
+        }
+    }
+    return false;
+}
 
 int main(){
     int vertices = 3;
@@ -35,5 +62,8 @@ int main(){
         1: 0 2
         2: 0 1
     */
+
+    isCyclic(adj, vertices) ? cout<<"Contains Cycle\n" : cout<< "No Cycle\n";
+
     return 0;
 }
