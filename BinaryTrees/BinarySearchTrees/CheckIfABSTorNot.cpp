92385b036d0010b7170117fe0950f9708d3c20a1
tusharx0809 <tusharbhatia0809@outlook.com>
2025-02-09 13:30:44 +0530

    added is valid bst function

--- BinaryTrees/BinarySearchTrees/CheckIfABSTorNot.cpp
+++ BinaryTrees/BinarySearchTrees/CheckIfABSTorNot.cpp
@@ -37,6 +37,16 @@ Node *insert(Node *root, int val){
     }
     return root;
 }
+bool isValidBstHelper(Node *root, long min, long max){
+    if(root == nullptr) return true;
+
+    if(root->data <= min || root->data >= max) return false;
+
+    return isValidBstHelper(root->left, min, root->data) && isValidBstHelper(root->right, root->data, max);
+}
+bool isValidBst(Node *root){
+    return isValidBstHelper(root, LONG_MIN, LONG_MAX);
+}
 void printInOrder(Node *root){
     if(root == nullptr){
         return;
@@ -44,7 +54,6 @@ void printInOrder(Node *root){
     printInOrder(root->left);
     cout<<root->data<<" ";
     printInOrder(root->right);
-
 }
 int main(){
     Node *root = new Node(50);
@@ -54,6 +63,36 @@ int main(){
         insert(root, val);
     }
 
-    printInOrder(root);
+    //      50
+    //     /  \
+    //    30   70
+    //   / \   / \
+    //  20 40 60 80
+
+    printInOrder(root); //20 30 40 50 60 70 80 
+    cout<<endl;
+
+    isValidBst(root) ? cout<<"Is BST\n" : cout<<"Is Not BST\n"; //Is BST
+
+    Node *root2 = new Node(50);
+
+    root2->left = new Node(30);
+    root2->right = new Node(70);
+    root2->left->left = new Node(40);
+    root2->left->right = new Node(20);
+    root2->right->right = new Node(80);
+    root2->right->left = new Node(60);
+
+    printInOrder(root2); //40 30 20 50 60 70 80
+    cout<<endl;
+
+    //      50
+    //     /  \
+    //    30   70
+    //   / \   / \
+    //  40 20 60 80
+
+    isValidBst(root2) ? cout<<"Is BST\n" : cout<<"Is Not BST\n"; //Is Not BST
+
     return 0;
 }
