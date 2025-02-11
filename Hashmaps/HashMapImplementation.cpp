ee09e239b58caa2c1a62ed57e6675ef4c17d4986
tusharx0809 <tusharbhatia0809@outlook.com>
2025-02-11 12:11:29 +0530

    added: added hashmap implementation

---
+++ Hashmaps/HashMapImplementation.cpp
@@ -0,0 +1,29 @@
+//CPP program to implement a hashmap
+
+#include<bits/stdc++.h>
+using namespace std;
+
+unordered_map<string, int> createMap(vector<string> &keys, vector<int> &values){
+    unordered_map<string, int> hashMap;
+
+    if(keys.size() != values.size()){
+        throw invalid_argument("Keys and Values must have same size");
+    }
+
+    for(int i=0; i<keys.size(); i++){
+        hashMap[keys[i]] = values[i]; 
+    }
+    return hashMap;
+}
+
+int main(){
+    vector<string> keys {"one", "two", "three"};
+    vector<int> values = {1, 2, 3};
+
+    unordered_map<string, int> hashMap = createMap(keys, values);
+
+    for(const auto &pair: hashMap){
+        cout << pair.first << ": "<< pair.second << endl;
+    }
+    return 0;
+}
